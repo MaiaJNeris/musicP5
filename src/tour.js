353672acc1013ed320f51ca956f0996f2b0703ae
MaiaJNeris <maiajneris@gmail.com>
2024-10-12 00:52:59 -0300

    advance1

--- src/tour.js
+++ src/tour.js
@@ -1,3 +1,9 @@
+// Pedir el nombre usuario
+let usuarioName = solicitarNombre();
+
+//Venta ticket 
+let ticket = 1
+
 // solicita nombre y valido
 function solicitarNombre() {
     let usuarioName = prompt("¡Bienvenido/a! Por favor, ingresa tu nombre (mínimo 3 letras):");
@@ -12,8 +18,20 @@ function solicitarNombre() {
     return usuarioName.toUpperCase();
 }
 
-// Pedir el nombre usuario
-let usuarioName = solicitarNombre();
-
 // Añadir el nombre y el ícono de tickets al span con id "welcome"
 document.getElementById('welcome').innerHTML = `¡Hola ${usuarioName}! <i class="fas fa-ticket-alt"></i> Bienvenido a nuestras fechas de tour.`;
+
+
+// FUNCTION TOUR.JS
+
+const getTickets = (ticket, location) => {
+    !(ticket === 0) ?
+        (   ticket--, 
+            swal(
+            `{"Oh no!",
+            "You are outta luck!, there are no more tickets for " +
+            ${location}, "info",}`
+        )) :
+        swal(`{"Sold!", "You have tickets to the" ${location}
+            " concert", "success"}`);
+}
